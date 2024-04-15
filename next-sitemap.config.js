76f1c8362344ed83165e066abac49ec315f6e390
Qubit1984 <qubit89@protonmail.com>
2024-04-16 06:08:24 +0800

    d

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,7 +4,13 @@ module.exports = {
   siteUrl: siteMetadata.siteUrl,
   generateRobotsTxt: true,
   sitemapSize: 7000,
-
+  sitemaps: [
+    {
+      loc: "https://www.japanesegrammar.tokyo/sitemap-0.xml",
+      lastmod: new Date(),
+      changefreq: "daily",
+    },
+  ],
   trailingSlash: true,
   additionalPaths: async () => {
     /* return ["/about", "/contact", "/blog"]; */
